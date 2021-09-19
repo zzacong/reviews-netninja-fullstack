3ad7fa2db2a70dc445936138c61e1c5748e1ade9
Zac Ong <61817066+zzacong@users.noreply.github.com>
2021-09-19 19:16:37 +1000

    fetch and display single review

--- pages/details/[id].jsx
+++ pages/details/[id].jsx
@@ -1,3 +1,21 @@
+import { useRouter } from 'next/router'
+import { useFetch } from '../../lib/hooks'
+
 export default function ReviewDetailsPage() {
-  return <div></div>
+  const router = useRouter()
+  const { id } = router.query
+
+  const { data: review, loading, error } = useFetch(`/reviews/${id}`)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error...</p>
+
+  return (
+    <div className="review-card">
+      <div className="rating">{review.rating}</div>
+      <h2>{review.title}</h2>
+      <small>console list</small>
+      <p>{review.body}</p>
+    </div>
+  )
 }
